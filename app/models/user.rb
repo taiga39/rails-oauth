f86c1c17e3f956e30be5a190c4e78d285873fda9
taiga39 <ishikawa.tigers@gmail.com>
2019-05-01 18:37:18 +0900

    devise setting(1)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  def self.create_unique_string
+    SecureRandom.uuid
+  end
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
